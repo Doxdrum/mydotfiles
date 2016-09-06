d651b9451e39bb54f39a7aff14a96564565220ca
Oscar Castillo-Felisola <o.castillo.felisola@gmail.com>
2016-09-06 17:20:46 -0300

    Adding the emacs-lisp files of conf

---
+++ emacs.d/elpa/org-gcal-20160805.2144/org-gcal-autoloads.el
@@ -0,0 +1,15 @@
+;;; org-gcal-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("org-gcal.el") (22476 34473 959457 709000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; org-gcal-autoloads.el ends here
