a196c59e086bcfad39510b9456ea8cebaf2231de
Oscar Castillo-Felisola <o.castillo.felisola@gmail.com>
2016-09-07 09:03:03 -0300

    LaTeX listings, diff-hl, and update of packages

---
+++ emacs.d/elpa/sage-shell-mode-20160906.535/sage-shell-mode-pkg.el
@@ -0,0 +1,9 @@
+(define-package "sage-shell-mode" "20160906.535" "A front-end for Sage Math"
+  '((cl-lib "0.5")
+    (deferred "0.3.1")
+    (emacs "24"))
+  :url "https://github.com/stakemori/sage-shell-mode" :keywords
+  '("sage" "math"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
