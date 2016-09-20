b74b4098401f9a57af578b5d4ddba29fc196b083
Oscar Castillo-Felisola <o.castillo.felisola@gmail.com>
2016-09-20 15:49:07 -0300

    Update of packages

--- emacs.d/elpa/sage-shell-mode-20160909.111/sage-shell-mode-pkg.el
+++
@@ -1,9 +0,0 @@
-(define-package "sage-shell-mode" "20160909.111" "A front-end for Sage Math"
-  '((cl-lib "0.5")
-    (deferred "0.3.1")
-    (emacs "24"))
-  :url "https://github.com/stakemori/sage-shell-mode" :keywords
-  '("sage" "math"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
