6402f90237691afad50fea318c7a16d755efb41b
Oscar Castillo-Felisola <o.castillo.felisola@gmail.com>
2016-10-22 11:51:45 -0300

    Org babel: sagemath and ipython

---
+++ emacs.d/elpa/dash-functional-20160615.1351/dash-functional-autoloads.el
@@ -0,0 +1,16 @@
+;;; dash-functional-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("dash-functional.el") (22539 31887 360538
+;;;;;;  560000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; dash-functional-autoloads.el ends here
