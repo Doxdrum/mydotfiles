2d8bb7a053486fe2c05870a4e2a3652402e988bc
Oscar Castillo-Felisola <o.castillo.felisola@gmail.com>
2017-01-17 05:09:19 -0300

    JAU (Just another update)

--- emacs.d/elpa/request-20161221.1711/request-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "request" "20161221.1711" "Compatible layer for URL request in Emacs" '((emacs "24") (cl-lib "0.5")))
---
+++ emacs.d/elpa/request-20170113.423/request-autoloads.el
@@ -0,0 +1,15 @@
+;;; request-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("request.el") (22652 50130 152424 333000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; request-autoloads.el ends here
