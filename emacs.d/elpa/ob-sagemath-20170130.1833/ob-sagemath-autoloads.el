c2445d7a109a8182575284cc966341186efa5bb1
Oscar Castillo-Felisola <o.castillo.felisola@gmail.com>
2017-01-31 05:49:34 -0300

    Daily update!

---
+++ emacs.d/elpa/ob-sagemath-20170130.1833/ob-sagemath-autoloads.el
@@ -0,0 +1,33 @@
+;;; ob-sagemath-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "ob-sagemath" "ob-sagemath.el" (22672 18694
+;;;;;;  693022 455000))
+;;; Generated autoloads from ob-sagemath.el
+
+(autoload 'ob-sagemath-execute-async "ob-sagemath" "\
+Execute current src code block. With prefix argument, evaluate all code in a
+buffer.
+
+\(fn ARG)" t nil)
+
+(autoload 'org-babel-execute:sage "ob-sagemath" "\
+
+
+\(fn BODY PARAMS)" nil nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("ob-sagemath-pkg.el") (22672 18694 984382
+;;;;;;  612000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; ob-sagemath-autoloads.el ends here
---
+++ emacs.d/elpa/ob-sagemath-20170130.1833/ob-sagemath-pkg.el
@@ -0,0 +1,9 @@
+(define-package "ob-sagemath" "20170130.1833" "org-babel functions for SageMath evaluation"
+  '((sage-shell-mode "0.0.8")
+    (s "1.8.0")
+    (emacs "24"))
+  :url "https://github.com/stakemori/ob-sagemath" :keywords
+  '("sagemath" "org-babel"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
