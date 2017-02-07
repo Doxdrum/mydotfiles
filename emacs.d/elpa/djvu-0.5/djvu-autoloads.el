e81368f0aa8fd9fbade88ff5542a72b7dedc0433
Oscar Castillo-Felisola <o.castillo.felisola@gmail.com>
2017-02-07 05:50:35 -0300

    Daily update!

---
+++ emacs.d/elpa/djvu-0.5/djvu-autoloads.el
@@ -0,0 +1,22 @@
+;;; djvu-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "djvu" "djvu.el" (22675 19141 361421 747000))
+;;; Generated autoloads from djvu.el
+
+(autoload 'djvu-find-file "djvu" "\
+Read and edit Djvu FILE on PAGE.
+If VIEW is non-nil start external viewer.
+
+\(fn FILE &optional PAGE VIEW)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; djvu-autoloads.el ends here
---
+++ emacs.d/elpa/djvu-0.5/djvu-pkg.el
@@ -0,0 +1,1 @@
+(define-package "djvu" "0.5" "Edit and view Djvu files via djvused" 'nil :stars '("") :url "http://elpa.gnu.org/packages/djvu.html" :keywords '("files" "wp"))
