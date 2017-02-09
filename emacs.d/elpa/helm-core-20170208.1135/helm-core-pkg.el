f3d7aaba14d2fd08d9e25cfcc9faa09d2936c680
Oscar Castillo-Felisola <o.castillo.felisola@gmail.com>
2017-02-09 12:19:20 -0300

    Daily update!

--- emacs.d/elpa/helm-core-20170208.1135/helm-core-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "helm-core" "20170208.1135" "Development files for Helm"
-  '((emacs "24.4")
-    (async "1.9"))
-  :url "https://emacs-helm.github.io/helm/")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
