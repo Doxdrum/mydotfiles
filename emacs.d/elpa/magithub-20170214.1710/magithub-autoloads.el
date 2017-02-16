731162ba863dd87a1fdcb5eba23e1c326ed83bb2
Oscar Castillo-Felisola <o.castillo.felisola@gmail.com>
2017-02-16 06:33:18 -0300

    Daily update!

---
+++ emacs.d/elpa/magithub-20170214.1710/magithub-autoloads.el
@@ -0,0 +1,17 @@
+;;; magithub-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("magithub-cache.el" "magithub-ci.el" "magithub-core.el"
+;;;;;;  "magithub-issue.el" "magithub-pkg.el" "magithub-proxy.el"
+;;;;;;  "magithub.el") (22691 65237 954483 947000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; magithub-autoloads.el ends here
