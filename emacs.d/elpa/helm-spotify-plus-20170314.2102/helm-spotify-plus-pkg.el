1de367d1d767f03e12451df725f0b9e84b9092d4
Oscar Castillo-Felisola <o.castillo.felisola@gmail.com>
2017-03-15 15:44:56 -0300

    Add: helm-spotify-plus

---
+++ emacs.d/elpa/helm-spotify-plus-20170314.2102/helm-spotify-plus-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "helm-spotify-plus" "20170314.2102" "Control Spotify search and select music with Helm." '((emacs "24.4") (helm "2.0.0") (multi "2.0.1")))
