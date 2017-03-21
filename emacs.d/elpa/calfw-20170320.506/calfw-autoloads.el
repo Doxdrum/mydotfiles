4634509f2affdb18cf8f47771453d9114e7b3c81
Oscar Castillo-Felisola <o.castillo.felisola@gmail.com>
2017-03-21 05:32:41 -0300

    Daily update!

---
+++ emacs.d/elpa/calfw-20170320.506/calfw-autoloads.el
@@ -0,0 +1,17 @@
+;;; calfw-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("calfw-cal.el" "calfw-howm.el" "calfw-ical.el"
+;;;;;;  "calfw-org.el" "calfw-pkg.el" "calfw.el") (22736 58581 351306
+;;;;;;  277000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; calfw-autoloads.el ends here
--- emacs.d/elpa/helm-spotify-plus-20170314.2102/helm-spotify-plus-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "helm-spotify-plus" "20170314.2102" "Control Spotify search and select music with Helm." '((emacs "24.4") (helm "2.0.0") (multi "2.0.1")))
