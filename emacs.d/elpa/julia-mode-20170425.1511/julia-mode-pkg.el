6ea182b5166cf93584d174cc05ca54e8eaaa2ba8
Oscar Castillo-Felisola <o.castillo.felisola@gmail.com>
2017-04-26 06:35:11 -0300

    reconf sage-shell-mode. Daily update

---
+++ emacs.d/elpa/julia-mode-20170425.1511/julia-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "julia-mode" "20170425.1511" "Major mode for editing Julia source code" 'nil :url "https://github.com/JuliaLang/julia" :keywords '("languages"))
