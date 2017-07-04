57fd7d880e9f1afe16fc7ca3b47594a30385238c
oscar <o.castillo.felisola@gmail.com>
2017-07-04 14:37:27 -0400

    Daily update...

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -2,3 +2,17 @@
 (package-initialize)
 (require 'org)
 (org-babel-load-file "~/Software/git.src/mydotfiles/emacs.d/configuration.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (twittering-mode spotify smex powerline pcache paradox paperless org2blog org-tree-slide org-ref org-projectile org-plus-contrib org-gcal ob-translate ob-sagemath ob-ipython magithub ivy-bibtex helm-swoop helm-spotify-plus helm-spotify helm-sage helm-projectile google-contacts gnuplot-mode ess epresent elpy djvu dired-k dired+ diff-hl counsel-projectile common-lisp-snippets calfw-gcal calfw bbdb auto-complete-sage auto-compile all-ext 2048-game))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
