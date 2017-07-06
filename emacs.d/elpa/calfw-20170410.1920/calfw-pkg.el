f3794cd9569578c34066ff79b3faed7c8f8620e5
oscar <o.castillo.felisola@gmail.com>
2017-07-06 08:58:52 -0400

    Daily update

--- emacs.d/elpa/calfw-20170410.1920/calfw-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "calfw" "20170410.1920" "Calendar view framework on Emacs" 'nil :url "https://github.com/kiwanami/emacs-calfw" :keywords
-  '("calendar"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
---
+++ emacs.d/elpa/ghub-20170702.512/ghub-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ghub" "20170702.512" "minuscule client for the Github API" '((emacs "25")) :commit "16c3300bb5d82b141aefa94c47ad9f97a58b0011" :url "https://github.com/tarsius/ghub" :keywords '("tools"))
