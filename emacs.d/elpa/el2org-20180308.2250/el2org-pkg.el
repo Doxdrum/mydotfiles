306ed2de784d6d0ccadc58b0a595a30789ee66b7
Oscar Castillo-Felisola <o.castillo.felisola@gmail.com>
2018-03-12 12:54:35 -0300

    Update:180312

--- emacs.d/elpa/el2org-20180308.2250/el2org-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "el2org" "20180308.2250" "Convert elisp file to org file" '((emacs "25.1")) :commit "eb3f89eb60e1207f74814913cb9eb25d76ebf3c0" :url "https://github.com/tumashu/el2org" :keywords '("convenience"))
---
+++ emacs.d/elpa/el2org-20180311.855/el2org-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "el2org" "20180311.855" "Convert elisp file to org file" '((emacs "25.1")) :commit "81f1c97db8911f5bdf92c729630ab509de4ec73f" :url "https://github.com/tumashu/el2org" :keywords '("convenience"))
