bcc88620e2c3e0d916adde712d999e90d55f5ccc
Oscar Castillo-Felisola <o.castillo.felisola@gmail.com>
2018-03-22 05:56:08 -0300

    180322 update

---
+++ emacs.d/elpa/helm-projectile-20180321.324/helm-projectile-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "helm-projectile" "20180321.324" "Helm integration for Projectile" '((helm "1.7.7") (projectile "0.14.0") (cl-lib "0.3")) :commit "4b6685a4f5e07d34329c3d30924a5a64a4742948" :url "https://github.com/bbatsov/helm-projectile" :keywords '("project" "convenience"))
