d0624b0174b5221d6c3734fbcd87cb022cbb1f30
Oscar Castillo-Felisola <o.castillo.felisola@gmail.com>
2018-04-05 04:41:46 -0300

    Daily update 180404

--- emacs.d/elpa/bbdb-20180317.2155/bbdb-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "bbdb" "20180317.2155" "The Insidious Big Brother Database for GNU Emacs" 'nil)
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
--- emacs.d/elpa/ghub+-20180318.1518/ghub+-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "ghub+" "20180318.1518" "a thick GitHub API client built on ghub" '((emacs "25") (ghub "1.2") (apiwrap "0.4")) :commit "c5021c2d30ed1738b616e74b628b140a3b8ada0b" :url "https://github.com/vermiculus/ghub-plus" :keywords '("extensions" "multimedia" "tools"))
