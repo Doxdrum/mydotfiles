c7ad5dca1cd3b417e8a81ea9974413f0b31fee59
Oscar Castillo-Felisola <o.castillo.felisola@gmail.com>
2018-04-26 14:45:30 -0300

    Update 180426

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -7,6 +7,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auth-source-save-behavior nil)
  '(custom-safe-themes
    (quote
     ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" default)))
@@ -22,3 +23,4 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(put 'scroll-left 'disabled nil)
