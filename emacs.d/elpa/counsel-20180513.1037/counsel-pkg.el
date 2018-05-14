e10f97896a140bfa4ce89929751f484c5635fc5e
Oscar Castillo-Felisola <o.castillo.felisola@gmail.com>
2018-05-14 04:37:17 -0400

    Update 180513

---
+++ emacs.d/elpa/counsel-20180513.1037/counsel-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "counsel" "20180513.1037" "Various completion functions using Ivy" '((emacs "24.3") (swiper "0.9.0")) :commit "62ded647d1f76bac9164047c965503e212633776" :keywords '("completion" "matching") :url "https://github.com/abo-abo/swiper")
---
+++ emacs.d/elpa/org-noter-20180512.858/org-noter-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "org-noter" "20180512.858" "A synchronized, Org-mode, document annotator" '((emacs "24.4") (cl-lib "0.6") (org "9.0")) :commit "1ee98e210454c02336bda86505480b22a89cb067" :keywords '("lisp" "pdf" "interleave" "annotate" "external" "sync" "notes" "documents" "org-mode") :url "https://github.com/weirdNox/org-noter")
