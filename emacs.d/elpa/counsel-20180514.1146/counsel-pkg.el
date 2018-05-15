337c8ebe627799833e3365018359e733f5235265
Oscar Castillo-Felisola <o.castillo.felisola@gmail.com>
2018-05-15 04:39:16 -0400

    Update 180515

---
+++ emacs.d/elpa/counsel-20180514.1146/counsel-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "counsel" "20180514.1146" "Various completion functions using Ivy" '((emacs "24.3") (swiper "0.9.0")) :commit "be6db350394c36cc09d886fa239e1fca491b050d" :keywords '("completion" "matching") :url "https://github.com/abo-abo/swiper")
