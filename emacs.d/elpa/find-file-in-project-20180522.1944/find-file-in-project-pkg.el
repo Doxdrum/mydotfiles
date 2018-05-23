b7c006c0eb5af6700eeab971aa8bdd17ba9a04b8
Oscar Castillo-Felisola <o.castillo.felisola@gmail.com>
2018-05-23 04:37:11 -0400

    Update 180523

---
+++ emacs.d/elpa/find-file-in-project-20180522.1944/find-file-in-project-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "find-file-in-project" "20180522.1944" "Find file/directory and review Diff/Patch/Commit efficiently everywhere" '((ivy "0.10.0") (emacs "24.3")) :commit "feec108e108c8a6f98b0373611e5655b9fd264b6" :keywords '("project" "convenience") :url "https://github.com/technomancy/find-file-in-project")
