ca4779ff5783620f871d7d91d556dee0027e0a43
Oscar Castillo-Felisola <o.castillo.felisola@gmail.com>
2018-05-29 04:39:55 -0400

    Update 180529

--- emacs.d/elpa/zerodark-theme-20180525.1307/zerodark-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "zerodark-theme" "20180525.1307" "A dark, medium contrast theme for Emacs" '((all-the-icons "2.0.0") (magit "2.8.0") (flycheck "29")) :commit "6189e9debc0843b593afcd3bf92633434a194de0" :keywords '("themes") :url "https://github.com/NicolasPetton/zerodark-theme")
