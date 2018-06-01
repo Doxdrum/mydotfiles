3f4a2c7796f1a589191bff7d6b5903cc44397be9
Oscar Castillo-Felisola <o.castillo.felisola@gmail.com>
2018-06-01 04:43:52 -0400

    Update 190601

---
+++ emacs.d/elpa/org-category-capture-20180531.1942/org-category-capture-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "org-category-capture" "20180531.1942" "Contextualy capture of org-mode TODOs." '((org "9.0.0") (emacs "24")) :commit "9d59490f6fb0e2cd20253e33c40c5e387d10968d" :keywords '("org-mode" "todo" "tools" "outlines") :url "https://github.com/IvanMalison/org-projectile")
---
+++ emacs.d/elpa/org-projectile-20180531.1942/org-projectile-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "org-projectile" "20180531.1942" "Repository todo management for org-mode" '((projectile "0.11.0") (dash "2.10.0") (emacs "24") (s "1.9.0") (org-category-capture "0.0.0")) :commit "9d59490f6fb0e2cd20253e33c40c5e387d10968d" :keywords '("org-mode" "projectile" "todo" "tools" "outlines") :url "https://github.com/IvanMalison/org-projectile")
