49a672130661cce635539b8a368a3da3972b9c1f
Oscar Castillo-Felisola <o.castillo.felisola@gmail.com>
2018-06-04 04:33:11 -0400

    Update 180604

--- emacs.d/elpa/ghub+-20180528.1158/ghub+-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "ghub+" "20180528.1158" "a thick GitHub API client built on ghub" '((emacs "25") (ghub "1.2") (apiwrap "0.4")) :commit "9d774bf31d1a55016b03a366f1623636c3fc8a14" :keywords '("extensions" "multimedia" "tools") :url "https://github.com/vermiculus/ghub-plus")
